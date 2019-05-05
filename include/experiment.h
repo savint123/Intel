5ca2f6a521893f1a282c53e2e94d08c66685bd03
sdsonawani <shubham.sonawani@asu.edu>
2019-05-05 16:35:08 -0700

    big fix

--- include/experiment.h
+++ include/experiment.h
@@ -13,8 +13,12 @@
 #include "geometry_msgs/Vector3.h"
 #include "geometry_msgs/Quaternion.h"
 #include "geometry_msgs/Twist.h"
+#include "geometry_msgs/PoseStamped.h"
 #include "tf/transform_datatypes.h"
 
+#include "std_srvs/Empty.h"
+#include "gazebo_msgs/SetModelState.h"
+#include "gazebo_msgs/GetModelState.h"
 
 
 class experiment_{
@@ -26,6 +30,10 @@ experiment_(ros::NodeHandle &nh){
     odom_sub = nh.subscribe("intel_robot/diff_drive_controller/odom",100,&experiment_::odomCallback, this);
     imu_sub = nh.subscribe("imu",100, &experiment_::imuCallback, this);
     vel_pub = nh.advertise<geometry_msgs::Twist>("intel_robot/diff_drive_controller/cmd_vel",100);
+    client = nh.serviceClient<std_srvs::Empty>("gazebo/reset_world");
+    client_set = nh.serviceClient<gazebo_msgs::SetModelState>("gazebo/set_model_state");
+    client_get = nh.serviceClient<gazebo_msgs::GetModelState>("gazebo/Get_model_state");
+    
 }
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr msg){
@@ -45,17 +53,70 @@ void imuCallback(const sensor_msgs::Imu::ConstPtr msg){
     tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
 }
 
-double run(double &velocity, double &distance, double &propotional, int &rate){
+void init_pose(){
+    *((double *)(&sim_current_pose)) = pose_x;
+}
+
+void reset_robot(){
+    set.request.model_state.model_name = "intel";
+    set.request.model_state.pose.position.x = 0;
+    set.request.model_state.pose.position.y = 0;
+    set.request.model_state.pose.position.z = 0;
+    set.request.model_state.pose.orientation.x = 0;
+    set.request.model_state.pose.orientation.y = 0;
+    set.request.model_state.pose.orientation.z= 0;
+    set.request.model_state.pose.orientation.w = 1;
+    set.request.model_state.twist.linear.x  = 0;
+    set.request.model_state.twist.linear.y  = 0;
+    set.request.model_state.twist.linear.z  = 0;
+    set.request.model_state.twist.angular.x  = 0;
+    set.request.model_state.twist.angular.y  = 0;
+    set.request.model_state.twist.angular.z  = 0;
+    set.request.model_state.reference_frame = "world";
+    client_set.call(set);
+    if(set.response.success){
+        ROS_INFO(" Model state is reinitialized at origin");
+    }
+
+
+
+}
+
+
+void gazebo_robot_state(){
+    geometry_msgs::PoseStamped grPose;
+   
+    get.request.model_name = "intel";
+    get.request.relative_entity_name = "base_link";
+
+    grPose.pose = get.response.pose;
+    if(get.response.success){
+    ROS_INFO("Model state is changed");
+    ROS_INFO("pose x: [%f], y: [%f], z: [%f]",grPose.pose.position.x,grPose.pose.position.y,grPose.pose.position.z);
+  }
+
+
+
+
+}
+
+
+void run(double &velocity, double &distance, double &propotional, int &rate){
     
     geometry_msgs::Twist msg;
     ref_yaw= 0;
-    
-    if(pose_x< distance){     
+    double goal_pose = sim_current_pose + distance;
+    std::cout<<"initial_pose: "<<sim_current_pose<<std::endl;
+    std::cout<<"current_pose: "<<pose_x<<std::endl;
+    if(pose_x < sim_current_pose){
+        throw std::invalid_argument("gazebo internal fault occured");
+    }
+    if(pose_x< goal_pose){     
         msg.linear.x = velocity;
 //******** adding propotional controller for straight line cruise control*******// 
         error = yaw - ref_yaw;
         if (error != 0){
-            error = -1.2 * propotional * error ;
+            error = - propotional * error ;
             msg.angular.z = (error);               
         }
         else{
@@ -63,55 +124,64 @@ double run(double &velocity, double &distance, double &propotional, int &rate){
         }
         vel_pub.publish(msg);   
     }
-
+    else {
+        run_ = false;
+    }
     // ROS_INFO("yaw: [%f] and error_yaw: [%f]", yaw, error);   
-    return pose_x;
        
+}
+void full_stop(){
+    geometry_msgs::Twist msg;
+    msg.linear.x = 0;
+    msg.angular.z = 0;
+    vel_pub.publish(msg);
+    ros::Duration(1).sleep();
 }
 
 void stop(double &value,double &propotional){
-    geometry_msgs::Twist msg;
+    geometry_msgs::Twist msg;    
     ref_yaw= 0;
     error = yaw - ref_yaw;
     if (error != 0){
-        error = -1.2 * propotional * error ;
+        error = - propotional * error ;
         msg.angular.z = (error );               
     }
     else{
         msg.angular.z = 0;
     }
     msg.linear.x = value; 
+    std::cout<<"current pitch is "<<pitch<<std::endl;
     // std::cout<<value<<std::endl;
     vel_pub.publish(msg);
 }
 
-double vel(double &pose, int &rate){
-    static double current_pose = 0;
-    static double old_pose = 0;
-    static double velocity = 0;
+// double vel(double &pose, int &rate){
+//     static double current_pose = 0;
+//     static double old_pose = 0;
+//     static double velocity = 0;
 
-    current_pose = pose;    
-    velocity = (current_pose - old_pose) * rate;
-    old_pose = current_pose;
+//     current_pose = pose;    
+//     velocity = (current_pose - old_pose) * rate;
+//     old_pose = current_pose;
 
-    return velocity;
+//     return velocity;
     
-}
+// }
 
-std::vector<double> decceleration_pattern(double &stop_time, int &number, double &velocity, int &opt){
+std::vector<double> decceleration_pattern(double &stop_time, int &number, int &opt){
      std::vector<double> decceleration ;
     //  nomralized_count devide the stop_time in required number of profiles
      double nomralized_count = stop_time /number;
     //  decceleration value given current velocity and stop time requirement
-     double decceleration_ = velocity / stop_time;
-    //  std::cout<<"current_velocity"<<std::endl<<velocity<<std::endl;
+     double decceleration_ =  vel_x / stop_time;
+     std::cout<<"current_velocity"<<std::endl<<vel_x<<std::endl;
     //  Twist msgs take velocity as input so we have to give all decceleration values in terms of velocities
     //  Considering current velocity and stop time we calculate decresing velocities in different time steps
      switch(opt){
         case 1:
             for(int i=1;i <= number; i++){
                 // straight line decceleration profiles (y = - m * x + c )
-                decceleration.push_back((-decceleration_) * (nomralized_count * i) + velocity);
+                decceleration.push_back((-decceleration_) * (nomralized_count * i) + vel_x);
                 // std::cout<<velocity/stop_time<<" : getting slope"<<std::endl;
                 // std::cout<<decceleration[i]<<std::endl;
             }
@@ -134,6 +204,19 @@ std::vector<double> decceleration_pattern(double &stop_time, int &number, double
 }
 
 
+public:
+const double sim_current_pose = 0;
+bool run_ = true;
+ros::ServiceClient client;
+ros::ServiceClient client_set;
+ros::ServiceClient client_get;
+
+std_srvs::Empty reset_world;
+gazebo_msgs::SetModelState set;
+gazebo_msgs::GetModelState get;
+
+
+
 private:
 
 ros::Publisher vel_pub ;
@@ -144,10 +227,7 @@ double ref_z, ref_yaw;
 double roll, pitch, yaw;
 double pose_x,pose_y, vel_x, vel_y, ang_z, acc_x, acc_y;
 double error, temp_ang_z =0;
-double deccl;
-
-
-
+double initial_pose;
 };
 
 
--- src/exp.cpp
+++ src/exp.cpp
@@ -1,58 +1,82 @@
 #include "experiment.h"
 
+
 int main(int argc,char **argv){
 
     ros::init(argc,argv,"experiment");
     ros::NodeHandle nh;
     experiment_ robot(nh);
 
+    
 
-    double desired_velocity =0.7, current_velocity, distance = 5.0, propotional = 0.8;
+    double desired_velocity = 0.7, current_velocity, distance = 5.0, propotional = 0.2;
     static double pose =0;
     //  rate (Hz) is used for defining loop frequency and profile generation 
     //  increment used to move from var to next var in profile
-    int rate = 5, increment = 0;
+    int rate = 100, increment = 0, count = 0;
     // get the deccleration profile in vector form and option for choosing between 
     // straght line, exponential and logarithmic 
     int option = 1;
     // stop time (sec) is varible to contrain within which robot needs to stop
-    double stop_time = 1;
+    double stop_time = 2;
     std::vector<double> profile ;
     // Boolian var for shifting from run to stop
-    // bool val = true;
-  
-    ros::Rate r(rate);   
-   
+    // bool run = true;
+    int current_sim = 0, sims = 2;
+    
 
-    while(ros::ok() ){   
+    ros::Rate r(100);   
+    
+
+    while(ros::ok() && current_sim<sims){   
         // if pose of robot is above 5 meters shift from run to stop mode 
-        if(pose<distance){
+        if(robot.run_){
+            if(count == 0){
+                robot.init_pose();
+                count = 1;
+            }
 
-            pose = robot.run(desired_velocity, distance, propotional, rate);
-            // std::cout<<pose<<std::endl;
-            current_velocity = robot.vel(pose,rate);
-            std::cout<<current_velocity<<std::endl;
+            robot.run(desired_velocity, distance, propotional, rate);
+            std::cout<<"Going to goal with simlation number "<<current_sim<<std::endl;
+            
+            // current_velocity = robot.vel(pose,rate);
+            // std::cout<<current_velocity<<std::endl;
             r.sleep();
             ros::spinOnce();
             }
 
-        else {
-            static int i = 0;
-             // Run profile generation only once by using following condition
-            if (i == 0){
-                profile = robot.decceleration_pattern(stop_time, rate, current_velocity, option);
-                // std::cout<<"executed "<<++i<<" times"<<std::endl;
-                }
-            i++;            
-            std::cout<<profile[increment]<<std::endl;
-            robot.stop(profile[increment],propotional);
-            increment++;
-            r.sleep();
-            ros::spinOnce();
-            if(increment == rate){
-                break;
-                }
+        else{
+                // static int i = 0;
+                // Run profile generation only once by using following condition
+                if (count == 1){
+                    profile = robot.decceleration_pattern(stop_time, rate, option);
+                    count = 0;
+                    // std::cout<<"executed "<<++i<<" times"<<std::endl;
+                    }
+                            
+                std::cout<<profile[increment]<<std::endl;
+                robot.stop(profile[increment],propotional);
+                increment++;
+                
+                r.sleep();
+                ros::spinOnce();
+
+                if(increment == rate)
+                    {
+                    robot.full_stop();                  
+                    robot.client.call(robot.reset_world);
+                    // count = 0;
+                    robot.reset_robot();
+                    increment = 0;
+                    robot.run_ = true;
+                    current_sim++;
+                    // sleep for 2 seconds
+                    ros::Duration(1).sleep();
+                    robot.gazebo_robot_state();
+                    
+                    }
             }
+            
         }     
     return 0;
 }
